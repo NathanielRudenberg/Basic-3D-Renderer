a7c7b329f9e981764b55682f33103c866be5a760
Nathaniel Rudenberg <nathaniel.rudenberg@gordon.edu>
2024-07-26 15:27:41 -0400

    Fix depth buffer value retrieval

--- Engine_OnLoop.cpp
+++ Engine_OnLoop.cpp
@@ -3,20 +3,7 @@
 void Engine::OnLoop() {
 	_window.clear();
 
-	float fps = 1.0f / elapsedTime;
-	string fpsString = "FPS: " + to_string(fps);
-	string mX = "X: " + to_string(_window.mX());
-	string mY = "Y: " + to_string(_window.mY());
-	string depth = "Depth: " + to_string( _window.getDepthBuffer()[(_window.mY() * SCREEN_WIDTH) + _window.mX()]);
-	string windowTitle = "Basic 3D Engine " + mX + ", " + mY;
-	if (SDL_GetRelativeMouseMode() == SDL_FALSE) {
-		windowTitle += ", " + depth;
-	}
-	windowTitle += ", " + fpsString;
-	_window.setTitle(windowTitle);
-
 	RowVector3f targetVec = _renderer.camera().getPos() + _renderer.camera().getForward();
-
 	Matrix4f cameraMatrix = getPointAtMatrix(_renderer.camera().getPos(), targetVec, _renderer.camera().getUp());
 	Matrix4f viewMatrix = cameraMatrix.inverse();
 
@@ -33,4 +20,16 @@ void Engine::OnLoop() {
 	//render(objects[2], viewMatrix, 35.0f, 0.0f, 4.0f);
 	//render(objects[3], viewMatrix, 0.0f, 0.0f, 1.0f);
 	//render(objects[4], viewMatrix, 0.0f, 0.0f, 1.0f);
+
+	float fps = 1.0f / elapsedTime;
+	string fpsString = "FPS: " + to_string(fps);
+	string mX = "X: " + to_string(_window.mX());
+	string mY = "Y: " + to_string(_window.mY());
+	string depth = "Depth: " + to_string( _window.getDepthBuffer()[(_window.mY() * SCREEN_WIDTH) + _window.mX()]);
+	string windowTitle = "Basic 3D Engine " + mX + ", " + mY;
+	if (SDL_GetRelativeMouseMode() == SDL_FALSE) {
+		windowTitle += ", " + depth;
+	}
+	windowTitle += ", " + fpsString;
+	_window.setTitle(windowTitle);
 }
